a674745f79b6a69c7c78c4b39a7ae98a895be302
ykhaito <youssef.khaito@flemingcollege.ca>
2024-01-16 09:23:46 -0500

    Modified both files in in the repo clone
    -Deleted one print statement from script1.py
    - Added one print statement to script2.py

--- script1.py
+++ script1.py
@@ -1,3 +1,2 @@
 # Prints line from Zyn of Python.
 print("Beautiful is better than ugly.")
-print("Simple is better than complex.")
--- script2.py
+++ script2.py
@@ -1,3 +1,4 @@
 # This is a good programming Advice
 print("Although practicality beats purity.")
 print("In the face of ambiguity, refuse the temptation to guess.")
+print("Beautiful is better than ugly.")
